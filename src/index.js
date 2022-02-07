5511cc2907b8cbfff8325ead3b41e074e6b416eb
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-07 15:10:47 -0800

    convert Kelvin to F.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log(request);
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+        console.log(response.main.temp);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${((response.main.temp - 273.15) * 9/5 + 32).toFixed(2)} degrees.`);
+    }
+  });
+});
