5c542237e5a9a4cba39d24b2a1baf88dc8b43faa
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-09 08:12:47 -0800

    refactor with fetch.

--- src/index.js
+++ src/index.js
@@ -4,28 +4,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import WeatherService from './js/weather-service.js';
 
-function clearFields() {
-  $('#location').val("");
-  $('.showErrors').text("");
-  $('.showHumidity').text("");
-  $('.showTemp').text("");
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+    let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(city);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
+    WeatherService.getWeather(city)
+      .then(function(response) {
+        getElements(response);
+      });
   });
 });
+
+// function clearFields() {
+//   $('#location').val("");
+//   $('.showErrors').text("");
+//   $('.showHumidity').text("");
+//   $('.showTemp').text("");
+// }
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     const city = $('#location').val();
+//     $('#location').val("");
+//     clearFields();
+//     let promise = WeatherService.getWeather(city);
+//     promise.then(function(response) {
+//       const body = JSON.parse(response);
+//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error}`);
+//     });
+//   });
+// });
 
 //     let promise = new Promise(function(resolve, reject) {
 //       let request = new XMLHttpRequest();
